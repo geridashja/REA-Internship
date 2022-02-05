e0d6ea6bb698cbed6f2618a42eabc5c5d0cbe87f
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2022-02-05 03:22:10 +0300

    req and get

--- Internship Assignment2/todo/src/main/java/com/todo/todo/controller/UserController.java
+++ Internship Assignment2/todo/src/main/java/com/todo/todo/controller/UserController.java
@@ -1,5 +1,33 @@
 package com.todo.todo.controller;
 
+
+import com.todo.todo.entity.Todo;
+import com.todo.todo.entity.User;
+import com.todo.todo.repository.TodoRepository;
+import com.todo.todo.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    //connecting to repos
+    private UserRepository userRepo;
+    private TodoRepository todoRepo;
+
+    public UserController(UserRepository userRepo, TodoRepository todoRepo) {
+        this.userRepo = userRepo;
+        this.todoRepo = todoRepo;
+    }
+
+    @GetMapping("/{userId}")
+    public User getUserbyId(@PathVariable Long userId){
+        return userRepo.findById(userId).orElseThrow(
+                () -> new NoSuchElementException());
+    }
 }
--- Internship Assignment2/todo/src/main/java/com/todo/todo/request/AddTodo.java
+++ Internship Assignment2/todo/src/main/java/com/todo/todo/request/AddTodo.java
@@ -1,4 +1,19 @@
 package com.todo.todo.request;
 
+
 public class AddTodo {
+
+    private String description;
+
+    public AddTodo(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
