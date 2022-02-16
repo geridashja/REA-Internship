06e147b3aa163ec5d5433283812e1492c4ae7d65
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2022-02-16 20:35:25 +0300

    delete todo-items

--- Internship Assignment2/todo/src/main/java/com/todo/todo/controller/UpdatesController.java
+++ Internship Assignment2/todo/src/main/java/com/todo/todo/controller/UpdatesController.java
@@ -10,16 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("userhome/completed/{}")
+@RequestMapping("userhome/completed/{id}")
 public class UpdatesController {
     @Autowired
     todouserrepo userRepository;
@@ -36,26 +33,14 @@ public class UpdatesController {
     }
 
     @GetMapping()
-    public String updatestatus(@ModelAttribute("todos") Todo todo){
+    public String updatestatus(@PathVariable Long id){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String name = auth.getName();
         todouser foruser = userRepository.findByUsername(name);
 
-        Optional<Todo> todo2 = Todorepository.findById(todo.getTodo_id());
+        Optional<Todo> todo2 = Todorepository.findById(id);
         Todo todo3 = todo2.get();
-        todo3.setStatus(true);
-        Todorepository.save(todo3);
-//        List<Todo> alltodosforuser = Todorepository.todos_foruser(foruser.getUser_id());
-//        for (Todo other : alltodosforuser) {
-//            if (other.getTodo_id() == todo3.getTodo_id()) {
-//                if(other.getStatus() == false){
-//                    todo3.setStatus(true);
-//                    Todorepository.delete(todo);
-//                    Todorepository.save(todo3);
-//                }
-//            }
-//        }
-//        Todorepository.updatetodostatus(true,todo3.getTodo_id());
+        Todorepository.updatetodostatus(true,todo3.getTodo_id());
         return "redirect:/userhome";
     }
 }
--- Internship Assignment2/todo/src/main/java/com/todo/todo/repository/TodoRepository.java
+++ Internship Assignment2/todo/src/main/java/com/todo/todo/repository/TodoRepository.java
@@ -17,10 +17,10 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     @Query(value="select distinct t.* from todostable t join usertable u on t.user_id =?1 where t.user_id = ?1",nativeQuery = true)
     List<Todo> todos_foruser(Long user_id);
 
-//    @Transactional
-//    @Modifying
-//    @Query(value="update todostable set status = ?1 where todo_id = ?2 AND status = false",nativeQuery = true)
-//    void updatetodostatus(boolean status, Long todo_id);
+    @Transactional
+    @Modifying
+    @Query(value="update todostable set status = ?1 where todo_id = ?2 AND status = false",nativeQuery = true)
+    void updatetodostatus(boolean status, Long todo_id);
 
 
 
